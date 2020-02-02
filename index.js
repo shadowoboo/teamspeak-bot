a87cb29f27a10862e2f2a7e284e999013c3b5805
shadowoboo <shadowoboo@gmail.com>
2020-02-03 02:40:16 +0800

    feat dndRoll and refactor about the bot

--- index.js
+++ index.js
@@ -36,47 +36,9 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 exports.__esModule = true;
-// //import with typescript
 var ts3_nodejs_library_1 = require("ts3-nodejs-library");
-// import * as axios from "axios";
 var fs = require("fs");
-// //import with javascript
-// //const { TeamSpeak } = require("ts3-nodejs-library")
-// // console.log(`start-------`);
-//create a new connection
-// TeamSpeak.connect({
-//     host: "127.0.0.1",
-//     // host: "140.121.80.175",
-//     protocol: QueryProtocol.RAW, //optional
-//     queryport: 10011, //optional
-//     serverport: 9987,
-//     username: "serveradmin",
-//     // password: "210310",
-//     password: "TtUzifTr",
-//     nickname: "NodeJS Query Framework",
-//     keepAlive: true,
-// }).then(async teamspeak => {
-//     // console.log(`connect result: `, teamspeak);
-//     const clients = await teamspeak.clientList({ client_type: 0 })
-//     clients.forEach(client => {
-//         console.log("Sending 'Hello!' Message to", client.nickname)
-//         client.message("Hello!")
-//     })
-// }).catch(e => {
-//     console.log("Catched an error!")
-//     console.error(e)
-// })
-// import { TeamSpeak } from "ts3-nodejs-library"
-// axios def
-// const aGet = axios.default.get;
-// const aPost = axios.default.post;
-// const aPatch = axios.default.patch;
-// const aDelete = axios.default.delete;
-// const axiosLocalConfig = {
-//     baseURL: 'https://127.0.0.1:10011'
-// }
 // get teamspeak account
-// let ts3Accounts;
 fs.readFile('./ts3Account.json', 'utf8', connectServer);
 /**
  * connoect to server after we get the bot account info
@@ -91,45 +53,57 @@ function connectServer(err, accounts) {
     var teamspeak = new ts3_nodejs_library_1.TeamSpeak(accountsConfigs[0]);
     var botInfo;
     var channelList;
-    teamspeak.connect();
+    // connect to target server by config
+    teamspeak.connect()["catch"](function (err) {
+        console.log("----------------------connect error--start------------------");
+        console.log(err);
+        console.log("----------------------connect error--end------------------");
+    });
+    // bot init
     teamspeak.on("ready", function () { return __awaiter(_this, void 0, void 0, function () {
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0: 
-                //teamspeak connected successfully
                 // get botInfo
-                return [4 /*yield*/, teamspeak.whoami().then(function (res) { return botInfo = res; })
-                    // get channelList
-                ];
+                return [4 /*yield*/, teamspeak.whoami().then(function (res) { return botInfo = res; })];
                 case 1:
-                    //teamspeak connected successfully
                     // get botInfo
                     _a.sent();
                     // get channelList
-                    return [4 /*yield*/, teamspeak.channelList().then(function (res) { return channelList = res; })
-                        // regist events
-                    ];
+                    return [4 /*yield*/, teamspeak.channelList().then(function (res) { return channelList = res; })];
                 case 2:
                     // get channelList
                     _a.sent();
+                    // notice chennal that bot login
+                    teamspeak.sendTextMessage(botInfo.client_channel_id, ts3_nodejs_library_1.TextMessageTargetMode.CHANNEL, botInfo.client_login_name + " is login now.");
                     // regist events
-                    Promise.all([
-                        teamspeak.registerEvent("server"),
-                        teamspeak.registerEvent("channel", 0),
-                        teamspeak.registerEvent("textserver"),
-                        teamspeak.registerEvent("textchannel"),
-                        teamspeak.registerEvent("textprivate"),
-                    ]);
+                    registTeamspeakEvents(teamspeak);
                     return [2 /*return*/];
             }
         });
     }); });
+    // bot close
+    teamspeak.on("close", function () { return __awaiter(_this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    console.log("disconnected, trying to reconnect...");
+                    return [4 /*yield*/, teamspeak.reconnect(-1, 1000)];
+                case 1:
+                    _a.sent();
+                    console.log("reconnected!");
+                    return [2 /*return*/];
+            }
+        });
+    }); });
+    // bot error
     teamspeak.on("error", function (err) {
         //teamspeak had an error
         console.log("----------------------teamspeak error--start------------------");
         console.log(err);
         console.log("----------------------teamspeak error--end------------------");
     });
+    // bot recive message then do something
     teamspeak.on("textmessage", function (ev) {
         var recieveMsg = ev.msg;
         var targetmode = ev.targetmode;
@@ -142,9 +116,12 @@ function connectServer(err, accounts) {
             return;
         }
         // bot say 0.0
-        if (recieveMsg.indexOf('?') === 1) {
-            teamspeak.sendTextMessage(channelGroupId, targetmode, "0.0?!");
-        }
+        // if (recieveMsg.indexOf('?') === 1) {
+        //     teamspeak.sendTextMessage(channelGroupId, targetmode, `0.0?!`)
+        // }
+        botSayNani(teamspeak, recieveMsg, channelGroupId, targetmode);
+        // dnd roll
+        botDice(teamspeak, recieveMsg, channelGroupId, targetmode, ev);
         // google search keyword
         if (recieveMsg.indexOf('google') === 1) {
             var keyword = recieveMsg.slice(8);
@@ -189,106 +166,147 @@ function connectServer(err, accounts) {
         // console.log(`${ev.invoker.nickname} just send the message "${ev.msg}"`)
     });
 }
-// aGet('/ts3Account.json', axiosLocalConfig)
-//     .then(accounts => {
-//         console.log(`accounts: `, accounts, typeof (accounts));
-//         // const teamspeak = new TeamSpeak(accounts[0])
-//     })
-// fetch('./ts3Account.json')
-//     // .then(accounts => ts3Accounts=accounts)
-//     .then(accounts => {
-//         console.log(`accounts: `, accounts, typeof (accounts));
-//         // const teamspeak = new TeamSpeak(accounts[0])
-//     })
-// const teamspeak = new TeamSpeak(ts3Accounts[0])
-// const teamspeak = new TeamSpeak({
-//     host: "127.0.0.1",
-//     // host: "140.121.80.175",
-//     protocol: QueryProtocol.RAW, //optional
-//     queryport: 10011, //optional
-//     serverport: 9987,
-//     username: "serveradmin",
-//     // password: "210310",
-//     password: "TtUzifTr",
-//     nickname: "Shadow Bot",
-//     keepAlive: true,
-// })
-// let botInfo: Whoami;
-// let channelList: TeamSpeakChannel[];
-// teamspeak.connect();
-// teamspeak.on("ready", async () => {
-//     //teamspeak connected successfully
-//     // get botInfo
-//     await teamspeak.whoami().then(res => botInfo = res)
-//     // get channelList
-//     await teamspeak.channelList().then(res => channelList = res)
-//     // regist events
-//     Promise.all([
-//         teamspeak.registerEvent("server"),
-//         teamspeak.registerEvent("channel", 0),
-//         teamspeak.registerEvent("textserver"),
-//         teamspeak.registerEvent("textchannel"),
-//         teamspeak.registerEvent("textprivate"),
-//     ])
-// })
-// teamspeak.on("error", () => {
-//     //teamspeak had an error
-// })
-// teamspeak.on("textmessage", ev => {
-//     const recieveMsg = ev.msg;
-//     const targetmode = ev.targetmode;
-//     const channelGroupId = ev.invoker.channelGroupId;
-//     // guard check
-//     if (recieveMsg.indexOf('!') !== 0) { return; }
-//     if (channelGroupId === null || channelGroupId === undefined) { return; }
-//     // bot say 0.0
-//     if (recieveMsg.indexOf('?') === 1) {
-//         teamspeak.sendTextMessage(channelGroupId, targetmode, `0.0?!`)
-//     }
-//     // google search keyword
-//     if (recieveMsg.indexOf('google') === 1) {
-//         const keyword = recieveMsg.slice(8);
-//         teamspeak.sendTextMessage(channelGroupId, targetmode, `[url=https://www.google.com/search?q=${keyword}&oq=${keyword}]${keyword}[/url]`);
-//     }
-//     // show channel list
-//     if (recieveMsg.indexOf('channelList') === 1 || recieveMsg.indexOf('頻道列表') === 1) {
-//         teamspeak.sendTextMessage(channelGroupId, targetmode, `channel id list: ${JSON.stringify(channelList, null, '  ')}`)
-//     }
-//     // show bot info
-//     if (recieveMsg.indexOf('botInfo') === 1 || recieveMsg.indexOf('機器人資訊') === 1) {
-//         teamspeak.sendTextMessage(channelGroupId, targetmode, JSON.stringify(botInfo, null, '  '))
-//     }
-//     // bot move to target channel
-//     if (
-//         recieveMsg.indexOf('botMove') === 1 ||
-//         recieveMsg.indexOf('機器人去') === 1
-//     ) {
-//         const sliceStart = recieveMsg.indexOf('botMove') === 1 ? 9 : 6;
-//         const targetChannelName = recieveMsg.slice(sliceStart);
-//         console.log(`targetChannelName: `, targetChannelName);
-//         const clientId = botInfo.client_id;
-//         const targetChannels = channelList.filter(channel => channel.name === targetChannelName);
-//         const targetChannelId = targetChannels && targetChannels.length > 0 ? targetChannels[0].cid : null;
-//         if (targetChannelId !== null) {
-//             teamspeak.sendTextMessage(botInfo.client_channel_id, 2, 'bot 走惹')
-//             teamspeak.clientMove(clientId, targetChannelId);
-//             teamspeak.sendTextMessage(targetChannelId, 2, 'bot 來惹')
-//         }
-//     }
-//     // client move to target channel
-//     if (
-//         recieveMsg.indexOf('move') === 1 ||
-//         recieveMsg.indexOf('去') === 1
-//     ) {
-//         const sliceStart = recieveMsg.indexOf('move') === 1 ? 6 : 3;
-//         const targetChannelName = recieveMsg.slice(sliceStart);
-//         console.log(`targetChannelName: `, targetChannelName);
-//         const clientId = ev.invoker.clid;
-//         const targetChannels = channelList.filter(channel => channel.name === targetChannelName);
-//         const targetChannelId = targetChannels && targetChannels.length > 0 ? targetChannels[0].cid : null;
-//         if (targetChannelId !== null) {
-//             teamspeak.clientMove(clientId, targetChannelId);
-//         }
-//     }
-//     // console.log(`${ev.invoker.nickname} just send the message "${ev.msg}"`)
-// })
+/**
+ * regist the Teamspeak events which want to be callback
+ * @param teamspeak Teamspeak Object with your config
+ */
+function registTeamspeakEvents(teamspeak) {
+    return Promise.all([
+        teamspeak.registerEvent("server"),
+        teamspeak.registerEvent("channel", 0),
+        teamspeak.registerEvent("textserver"),
+        teamspeak.registerEvent("textchannel"),
+        teamspeak.registerEvent("textprivate"),
+    ]);
+}
+/**
+ * bot command format sample: !google Something
+ * bot command format : 'cmdSymbol''cmd' 'param'
+ * @param textMsgObj the response object when teamspeak event "textmessage" effcet
+ */
+function botOnRecieveMsg(textMsgObj) {
+    // set common const
+    var recieveMsg = textMsgObj.msg.trim();
+    var channelGroupId = textMsgObj.invoker.channelGroupId;
+    // set bot config
+    var cmdSymbol = '!';
+    // guard check
+    if (recieveMsg.indexOf(cmdSymbol) !== 0) {
+        return;
+    } // bot cmd symbol check
+    if (channelGroupId === null || channelGroupId === undefined) {
+        return;
+    } // channel id exist
+}
+var BotCmd = /** @class */ (function () {
+    function BotCmd(teamspeak, cmdSymbol, cmdKeywords, cmdParam, cmdResponse) {
+        this.cmdSymbol = '!';
+        this.cmdKeywords = [];
+        this.cmdParam = [];
+        this.cmdResponse = function (recieveMsg) { };
+        this.cmdSymbol = cmdSymbol;
+        this.cmdKeywords = cmdKeywords;
+        this.cmdParam = cmdParam;
+        this.cmdResponse = cmdResponse;
+        this.teamspeak = teamspeak;
+    }
+    return BotCmd;
+}());
+function botSayNani(teamspeak, recieveMsg, channelGroupId, targetmode) {
+    // set BotCmd Obj
+    var botCmd = new BotCmd(teamspeak, '!', ['?'], [], function (recieveMsg) {
+        teamspeak.sendTextMessage(channelGroupId, targetmode, "0.0?!");
+    });
+    // parse cmd line and parse msg
+    var regexpRule = new RegExp("(^" + botCmd.cmdSymbol + ")(\\" + botCmd.cmdKeywords + ")");
+    var isEffect = regexpRule.test(recieveMsg);
+    var textParse = isEffect ? recieveMsg.match(regexpRule) : null;
+    var recieveParam = textParse ? textParse[3] : null;
+    // response
+    if (isEffect) {
+        botCmd.cmdResponse(recieveMsg);
+    }
+}
+/**
+ * bot to dice with dnd rule
+ * @param teamspeak
+ * @param recieveMsg
+ * @param channelGroupId
+ * @param targetmode
+ * @param ev
+ */
+function botDice(teamspeak, recieveMsg, channelGroupId, targetmode, ev) {
+    var askerName = ev.invoker.nickname;
+    // set BotCmd Obj
+    var botCmd = new BotCmd(teamspeak, '!', ['roll'], // TODO: 讓正則表達支援 array 關鍵字判斷, 任一
+    [], function (msg) {
+        // dnd roll parse rule
+        var regexpRule = /(?:\((?<number>\d+)d(?<value>\d+)\)(?:(?<prefix>[\+\-])(?<bonus>\d+))?)/;
+        var isEffect = regexpRule.test(recieveMsg);
+        var textParse = isEffect ? recieveMsg.match(regexpRule) : null;
+        // teamspeak.sendTextMessage(channelGroupId, targetmode, msg)
+        // teamspeak.sendTextMessage(channelGroupId, targetmode, textParse[0])
+        // teamspeak.sendTextMessage(channelGroupId, targetmode, (msg===textParse[0])?'true':'false')
+        // teamspeak.sendTextMessage(channelGroupId, targetmode, JSON.stringify(textParse, null, '  '))
+        if (!textParse || msg !== textParse[0] || !textParse.groups) {
+            return;
+        }
+        // dice qty
+        var theNumber = Number(textParse.groups.number) || 0;
+        // dice range 1~n
+        var theValue = Number(textParse.groups.value) || 0;
+        // 計算符號
+        var thePrefix = textParse.groups.prefix ? textParse.groups.prefix : null;
+        // bonus number
+        var theBonus = textParse.groups.bonus ? Number(textParse.groups.bonus) : 0;
+        // calculate the random result
+        var res = dndBasicDice(theNumber, theValue);
+        switch (thePrefix) {
+            case '+':
+                res += theBonus;
+                break;
+            case '-':
+                res -= theBonus;
+                break;
+            default:
+                break;
+        }
+        // send Message
+        var sendMsg = "\n                [size=24][color=blue]" + askerName + "[/color] \u8981\u6C42\u64F2\u9AB0 " + msg + "[/size] : [size=32][color=#fa36cc]" + res.toString() + "[/color][/size]\n            ";
+        teamspeak.sendTextMessage(channelGroupId, targetmode, sendMsg);
+    });
+    // parse cmd line and parse msg
+    var regexpRule = new RegExp("(^" + botCmd.cmdSymbol + ")(" + botCmd.cmdKeywords + ") (.*)");
+    var isEffect = regexpRule.test(recieveMsg);
+    var textParse = isEffect ? recieveMsg.match(regexpRule) : null;
+    var recieveParam = textParse ? textParse[3] : null;
+    // response
+    if (isEffect) {
+        // teamspeak.sendTextMessage(channelGroupId, targetmode, JSON.stringify(textParse, null, '  '))
+        botCmd.cmdResponse(recieveParam);
+    }
+}
+/**
+ * random calculate with dnd rule
+ * @param diceQty
+ * @param diceRange
+ * @param beginningNumber
+ */
+function dndBasicDice(diceQty, diceRange, beginningNumber) {
+    if (beginningNumber === void 0) { beginningNumber = 0; }
+    var randomNumber = Math.floor((Math.random() * diceRange) + 1);
+    // console.log(randomNumber);
+    if (diceQty > 0) {
+        return dndBasicDice(diceQty - 1, diceRange, beginningNumber + randomNumber);
+    }
+    return beginningNumber;
+}
+// let totalArr = Array(12).fill(0);
+// for(let i=1;i<=10000;i++){
+//     let res = Math.floor((dndBasicDice(1,100,0)-1)/10);
+//     res<0 ? totalArr[10]++ :
+//     res>10 ? totalArr[11]++ :
+//     totalArr[res]++;
+//     // totalArr = totalArr.concat([]);
+// }
+// console.log(`totalArr: `,totalArr);
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import { TeamSpeak, QueryProtocol, TeamSpeakChannel, ConnectionParams } from "ts3-nodejs-library"
+import { TeamSpeak, QueryProtocol, TeamSpeakChannel, ConnectionParams, TextMessageTargetMode } from "ts3-nodejs-library"
 import { Whoami } from "ts3-nodejs-library/lib/types/ResponseTypes";
 import * as fs from "fs";
+import { TextMessage } from "ts3-nodejs-library/lib/types/Events";
 
 // get teamspeak account
 fs.readFile('./ts3Account.json', 'utf8', connectServer)
@@ -12,32 +13,48 @@ fs.readFile('./ts3Account.json', 'utf8', connectServer)
  */
 function connectServer(err: NodeJS.ErrnoException | null, accounts: string) {
     if (err) throw err;
+
     const accountsConfigs: Partial<ConnectionParams>[] = JSON.parse(accounts);
     const teamspeak = new TeamSpeak(accountsConfigs[0])
+
     let botInfo: Whoami;
     let channelList: TeamSpeakChannel[];
 
-    teamspeak.connect();
+    // connect to target server by config
+    teamspeak.connect().catch(err=>{
+        console.log(`----------------------connect error--start------------------`);
+        console.log(err);
+        console.log(`----------------------connect error--end------------------`);
+    });
 
+    // bot init
     teamspeak.on("ready", async () => {
-        //teamspeak connected successfully
 
         // get botInfo
-        await teamspeak.whoami().then(res => botInfo = res)
+        await teamspeak.whoami().then(res => botInfo = res);
 
         // get channelList
-        await teamspeak.channelList().then(res => channelList = res)
+        await teamspeak.channelList().then(res => channelList = res);
+
+        // notice chennal that bot login
+        teamspeak.sendTextMessage(
+            botInfo.client_channel_id,
+            TextMessageTargetMode.CHANNEL,
+            `${botInfo.client_login_name} is login now.`
+        );
 
         // regist events
-        Promise.all([
-            teamspeak.registerEvent("server"),
-            teamspeak.registerEvent("channel", 0),
-            teamspeak.registerEvent("textserver"),
-            teamspeak.registerEvent("textchannel"),
-            teamspeak.registerEvent("textprivate"),
-        ])
+        registTeamspeakEvents(teamspeak);
     })
 
+    // bot close
+    teamspeak.on("close", async()=>{
+        console.log("disconnected, trying to reconnect...")
+        await teamspeak.reconnect(-1, 1000)
+        console.log("reconnected!")
+    })
+
+    // bot error
     teamspeak.on("error", (err) => {
         //teamspeak had an error
         console.log(`----------------------teamspeak error--start------------------`);
@@ -46,7 +63,7 @@ function connectServer(err: NodeJS.ErrnoException | null, accounts: string) {
     })
 
 
-
+    // bot recive message then do something
     teamspeak.on("textmessage", ev => {
         const recieveMsg = ev.msg;
         const targetmode = ev.targetmode;
@@ -57,9 +74,13 @@ function connectServer(err: NodeJS.ErrnoException | null, accounts: string) {
         if (channelGroupId === null || channelGroupId === undefined) { return; }
 
         // bot say 0.0
-        if (recieveMsg.indexOf('?') === 1) {
-            teamspeak.sendTextMessage(channelGroupId, targetmode, `0.0?!`)
-        }
+        // if (recieveMsg.indexOf('?') === 1) {
+        //     teamspeak.sendTextMessage(channelGroupId, targetmode, `0.0?!`)
+        // }
+        botSayNani(teamspeak, recieveMsg, channelGroupId, targetmode);
+
+        // dnd roll
+        botDice(teamspeak, recieveMsg, channelGroupId, targetmode, ev);
 
         // google search keyword
         if (recieveMsg.indexOf('google') === 1) {
@@ -116,4 +137,195 @@ function connectServer(err: NodeJS.ErrnoException | null, accounts: string) {
         // console.log(`${ev.invoker.nickname} just send the message "${ev.msg}"`)
     })
 }
+
+/**
+ * regist the Teamspeak events which want to be callback
+ * @param teamspeak Teamspeak Object with your config
+ */
+function registTeamspeakEvents(teamspeak: TeamSpeak) {
+    return Promise.all([
+        teamspeak.registerEvent("server"),
+        teamspeak.registerEvent("channel", 0),
+        teamspeak.registerEvent("textserver"),
+        teamspeak.registerEvent("textchannel"),
+        teamspeak.registerEvent("textprivate"),
+    ])
+}
+
+/**
+ * bot command format sample: !google Something
+ * bot command format : 'cmdSymbol''cmd' 'param'
+ * @param textMsgObj the response object when teamspeak event "textmessage" effcet
+ */
+function botOnRecieveMsg(textMsgObj: TextMessage) {
+
+    // set common const
+    const recieveMsg = textMsgObj.msg.trim();
+    const channelGroupId = textMsgObj.invoker.channelGroupId;
+
+    // set bot config
+    const cmdSymbol = '!';
+
+    // guard check
+    if (recieveMsg.indexOf(cmdSymbol) !== 0) { return; } // bot cmd symbol check
+    if (channelGroupId === null || channelGroupId === undefined) { return; } // channel id exist
+
+
+
+}
+
+class BotCmd {
+    cmdSymbol: string = '!';
+    cmdKeywords: string[] = [];
+    cmdParam: string[] = [];
+    cmdResponse: Function = function (recieveMsg: string) { };
+    teamspeak: TeamSpeak;
+
+    constructor(
+        teamspeak: TeamSpeak,
+        cmdSymbol: string,
+        cmdKeywords: string[],
+        cmdParam: string[],
+        cmdResponse: Function
+    ) {
+        this.cmdSymbol = cmdSymbol;
+        this.cmdKeywords = cmdKeywords;
+        this.cmdParam = cmdParam;
+        this.cmdResponse = cmdResponse;
+        this.teamspeak = teamspeak;
+    }
+}
+
+function botSayNani(teamspeak: TeamSpeak, recieveMsg: string, channelGroupId: number, targetmode: number) {
+
+    // set BotCmd Obj
+    let botCmd = new BotCmd(
+        teamspeak,
+        '!',
+        ['?'],
+        [],
+        function (recieveMsg: string) {
+            teamspeak.sendTextMessage(channelGroupId, targetmode, `0.0?!`)
+        }
+    )
+
+    // parse cmd line and parse msg
+    const regexpRule = new RegExp(`(^${botCmd.cmdSymbol})(\\${botCmd.cmdKeywords})`)
+    const isEffect = regexpRule.test(recieveMsg);
+    const textParse = isEffect ? recieveMsg.match(regexpRule) : null;
+    const recieveParam = textParse ? textParse[3] : null;
+
+    // response
+    if (isEffect) {
+        botCmd.cmdResponse(recieveMsg);
+    }
+
+}
+
+
+/**
+ * bot to dice with dnd rule
+ * @param teamspeak 
+ * @param recieveMsg 
+ * @param channelGroupId 
+ * @param targetmode 
+ * @param ev 
+ */
+function botDice(teamspeak: TeamSpeak, recieveMsg: string, channelGroupId: number, targetmode: number, ev: TextMessage) {
+ 
+    const askerName = ev.invoker.nickname;
+
+    // set BotCmd Obj
+    let botCmd = new BotCmd(
+        teamspeak,
+        '!',
+        ['roll'], // TODO: 讓正則表達支援 array 關鍵字判斷, 任一
+        [],
+        function (msg: string) {
+
+            // dnd roll parse rule
+            const regexpRule = /(?:\((?<number>\d+)d(?<value>\d+)\)(?:(?<prefix>[\+\-])(?<bonus>\d+))?)/;
+            const isEffect = regexpRule.test(recieveMsg);
+            const textParse = isEffect ? recieveMsg.match(regexpRule) : null;
+
+            // teamspeak.sendTextMessage(channelGroupId, targetmode, msg)
+            // teamspeak.sendTextMessage(channelGroupId, targetmode, textParse[0])
+            // teamspeak.sendTextMessage(channelGroupId, targetmode, (msg===textParse[0])?'true':'false')
+            // teamspeak.sendTextMessage(channelGroupId, targetmode, JSON.stringify(textParse, null, '  '))
+
+            if (!textParse || msg !== textParse[0] || !textParse.groups) { return; }
+
+            // dice qty
+            const theNumber = Number(textParse.groups.number) || 0;
+            // dice range 1~n
+            const theValue = Number(textParse.groups.value) || 0;
+            // 計算符號
+            const thePrefix = textParse.groups.prefix ? textParse.groups.prefix : null;
+            // bonus number
+            const theBonus = textParse.groups.bonus ? Number(textParse.groups.bonus) : 0;
+
+            // calculate the random result
+            let res = dndBasicDice(theNumber, theValue)
+            switch (thePrefix) {
+                case '+':
+                    res += theBonus;
+                    break;
+                case '-':
+                    res -= theBonus;
+                    break;
+                default:
+                    break;
+            }
+
+            // send Message
+            const sendMsg = `
+                [size=24][color=blue]${askerName}[/color] 要求擲骰 ${msg}[/size] : [size=32][color=#fa36cc]${res.toString()}[/color][/size]
+            `;
+            teamspeak.sendTextMessage(channelGroupId, targetmode, sendMsg)
+        }
+    )
+
+    // parse cmd line and parse msg
+    const regexpRule = new RegExp(`(^${botCmd.cmdSymbol})(${botCmd.cmdKeywords}) (.*)`)
+    const isEffect = regexpRule.test(recieveMsg);
+    const textParse = isEffect ? recieveMsg.match(regexpRule) : null;
+    const recieveParam = textParse ? textParse[3] : null;
+
+    
+    // response
+    if (isEffect) {
+        // teamspeak.sendTextMessage(channelGroupId, targetmode, JSON.stringify(textParse, null, '  '))
+        botCmd.cmdResponse(recieveParam);
+    }
+
+}
+
+/**
+ * random calculate with dnd rule
+ * @param diceQty 
+ * @param diceRange 
+ * @param beginningNumber 
+ */
+function dndBasicDice(diceQty: number, diceRange: number, beginningNumber: number = 0): number {
+    const randomNumber = Math.floor((Math.random() * diceRange) + 1);
+    // console.log(randomNumber);
+
+    if (diceQty > 0) {
+        return dndBasicDice(diceQty - 1, diceRange, beginningNumber + randomNumber)
+    }
+    return beginningNumber;
+}
+
+
+// let totalArr = Array(12).fill(0);
+
+// for(let i=1;i<=10000;i++){
+//     let res = Math.floor((dndBasicDice(1,100,0)-1)/10);
+//     res<0 ? totalArr[10]++ :
+//     res>10 ? totalArr[11]++ :
+//     totalArr[res]++;
+//     // totalArr = totalArr.concat([]);
+// }
+
+// console.log(`totalArr: `,totalArr);
 
